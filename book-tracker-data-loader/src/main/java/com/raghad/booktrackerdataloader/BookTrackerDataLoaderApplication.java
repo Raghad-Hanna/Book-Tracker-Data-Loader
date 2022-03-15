06a20f83783e9d11d63d17cb94483ab6e7b3c5b2
Raghad-Hanna <rgh.26d@gmail.com>
2022-03-15 00:36:14 -0700

    project setup with raw data to process & load

---
+++ book-tracker-data-loader/src/main/java/com/raghad/booktrackerdataloader/BookTrackerDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.raghad.booktrackerdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackerDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTrackerDataLoaderApplication.class, args);
+	}
+
+}
---
+++ book-tracker-data-loader/src/test/java/com/raghad/booktrackerdataloader/BookTrackerDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghad.booktrackerdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
